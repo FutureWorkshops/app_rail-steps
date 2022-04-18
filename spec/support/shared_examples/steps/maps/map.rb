03d1c297bd074565c2d0e798c00b5548324a87fd
Matt Brooke-Smith <matt@futureworkshops.com>
2022-04-18 15:44:47 +0100

    Update ar_maps_map_item id to string

--- spec/support/shared_examples/steps/maps/map.rb
+++ spec/support/shared_examples/steps/maps/map.rb
@@ -5,7 +5,7 @@ shared_examples_for "maps_map" do |_param|
     context "text" do
       let(:result) { subject.ar_maps_map_item(text: "London", latitude: 51.5, longitude: 0.51) }
 
-      it { expect(result[:id]).to eq 1 }
+      it { expect(result[:id]).to eq '1' }
       it { expect(result[:text]).to eq "London" }
       it { expect(result[:latitude]).to eq 51.5 }
       it { expect(result[:longitude]).to eq 0.51 }
@@ -14,7 +14,7 @@ shared_examples_for "maps_map" do |_param|
     context "coordinates as string" do
       let(:result) { subject.ar_maps_map_item(text: "London", latitude: '51.5', longitude: '0.51') }
 
-      it { expect(result[:id]).to eq 1 }
+      it { expect(result[:id]).to eq '1' }
       it { expect(result[:text]).to eq "London" }
       it { expect(result[:latitude]).to eq 51.5 }
       it { expect(result[:longitude]).to eq 0.51 }
