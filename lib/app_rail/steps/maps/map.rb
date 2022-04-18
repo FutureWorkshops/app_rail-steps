edf95e847eccc610325fc70406718e7f27f7128a
Matt Brooke-Smith <matt@futureworkshops.com>
2022-04-18 14:00:08 +0100

    Add to_f to maps location

--- lib/app_rail/steps/maps/map.rb
+++ lib/app_rail/steps/maps/map.rb
@@ -5,7 +5,7 @@ module AppRail
     module Maps
       module Map
         def ar_maps_map_item(text:, latitude:, longitude:, id: self.id, detail_text: nil)
-          { id: id, text: text, latitude: latitude, longitude: longitude, detailText: detail_text }.compact
+          { id: id, text: text, latitude: latitude.to_f, longitude: longitude.to_f, detailText: detail_text }.compact
         end
       end
     end
--- spec/support/shared_examples/steps/maps/map.rb
+++ spec/support/shared_examples/steps/maps/map.rb
@@ -10,5 +10,14 @@ shared_examples_for "maps_map" do |_param|
       it { expect(result[:latitude]).to eq 51.5 }
       it { expect(result[:longitude]).to eq 0.51 }
     end
+
+    context "coordinates as string" do
+      let(:result) { subject.ar_maps_map_item(text: "London", latitude: '51.5', longitude: '0.51') }
+
+      it { expect(result[:id]).to eq 1 }
+      it { expect(result[:text]).to eq "London" }
+      it { expect(result[:latitude]).to eq 51.5 }
+      it { expect(result[:longitude]).to eq 0.51 }
+    end
   end
 end
